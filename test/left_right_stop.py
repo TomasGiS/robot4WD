82cf7a2121abc37dd4912a3bca16b32388c61b0f
TomasGiS.rpiSense <me@tomasgis.cat>
2017-12-29 01:27:49 +0000

    Test updated to stop left motors

--- test/left_right_stop.py
+++ test/left_right_stop.py
@@ -4,12 +4,22 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setwarnings(False)
 
+#Right
 GPIO.setup(33, GPIO.OUT)
 GPIO.setup(29, GPIO.OUT)
 GPIO.setup(31, GPIO.OUT)
+
+#Left
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
 GPIO.output(29, False)
 GPIO.output(31, False)
 
 GPIO.output(33,False)
 
+GPIO.output(16,False)
+GPIO.output(18,False)
+
+GPIO.output(12,False)
